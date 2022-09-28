964aa901c424407a44fd75cb8bdb534f3c7bb3e5
Erick Clinton <erickleite2009321@gmail.com>
2022-09-28 08:49:41 -0300

    Criando a função de remover

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,10 +9,7 @@ import { useState } from 'react'
 //poderia colocar props e toda vez escrever props.author.
 
 export function Post({author, publishedAt,content}){
-    const [comment,setComments] = useState([
-        'Posta muito bacana,hein?!'
-        
-    ])
+    
     const publishedDateFormatted= format(publishedAt,"d 'de' LLLL 'às' HH:mm'h'",{
         locale:ptBR
     })
@@ -21,8 +18,12 @@ export function Post({author, publishedAt,content}){
         locale:ptBR,
         addSuffix:true
     })
+    const [comment,setComments] = useState([
+        'Posta muito bacana,hein?!'
+        
+    ])
+
     const [newCommentText,setNewCommentText]=useState('')
-    
 
     function handleCreateNewComment(){
         event.preventDefault()
@@ -35,6 +36,15 @@ export function Post({author, publishedAt,content}){
     function handleNewCommentChange(){
         setNewCommentText(event.target.value)
     }
+    
+    function deleteComment(commentToDelete){
+        //Nesta função, estpa chamando a lista comment e filtrando ela. caso o comment seja diferente do parametro passado, se mantém na lista
+        const commentsWithoudDeletedOne=comment.filter(comment=>{
+            return comment !== commentToDelete
+        })
+        setComments(commentsWithoudDeletedOne);
+    }
+
     return( 
         <article className={styles.post}>
             <header>
@@ -53,9 +63,9 @@ export function Post({author, publishedAt,content}){
             <div className={styles.content}>
                 {content.map(line=>{
                         if(line.type==='paragraph'){
-                            return <p>{line.content}</p>
+                            return <p key={line.content}>{line.content}</p>
                         }else if(line.type==='link'){
-                            return <p><a href="">{line.content}</a></p>
+                            return <p key={line.content}><a href="">{line.content}</a></p>
                         }
                     })}
             </div>
@@ -75,7 +85,13 @@ export function Post({author, publishedAt,content}){
 
             <div className={styles.commentList}>
                 {comment.map(comment=>{
-                    return <Comment content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment}
+                            content={comment} 
+                            onDeleteComment={deleteComment}
+                        />
+                    )
                 })}
             </div>
         </article>
